4b4f2ca95a353ccea59f904ffa90650fc4165d98
vipadaaa <vipadaaa@DESKTOP-7MPP1BS>
2021-09-09 13:17:10 +0700

    Add project files.

---
+++ week5-2/week5-2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter your number : ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
